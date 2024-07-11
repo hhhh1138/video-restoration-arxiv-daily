f0c6087addbeba3c77e76c34275bee2cfa7052c5
Yutong Wang <55480809+hhhh1138@users.noreply.github.com>
2024-07-11 16:35:04 +0800

    Update daily_arxiv.py

--- daily_arxiv.py
+++ daily_arxiv.py
@@ -332,8 +332,8 @@ def json_to_md(recent_trend_path,
         f.write("  </ol>\n")
         f.write("</details>\n\n")
 
-        f.write("[>>>> Each Paper Analysis (by AI) <<<<](https://github.com/hhhh1138/video-restoration-arxiv-daily/blob/main/analysis_by_ai.md) \n\n")
-        f.write("[Web Page](https://liutaocode.github.io/talking-face-arxiv-daily/) ([Scrape Code](https://github.com/liutaocode/talking-face-arxiv-daily)) \n\n")
+        # f.write("[>>>> Each Paper Analysis (by AI) <<<<](https://github.com/hhhh1138/video-restoration-arxiv-daily/blob/main/analysis_by_ai.md) \n\n")
+        # f.write("[Web Page](https://liutaocode.github.io/talking-face-arxiv-daily/) ([Scrape Code](https://github.com/liutaocode/talking-face-arxiv-daily)) \n\n")
         
         #Add: table of contents
         if use_tc == True:
@@ -397,20 +397,20 @@ def json_to_md(recent_trend_path,
             # we don't like long string, break it!
             f.write((f"[contributors-shield]: https://img.shields.io/github/"
                      f"contributors/liutaocode/talking-face-arxiv-daily.svg?style=for-the-badge\n"))
-            f.write((f"[contributors-url]: https://github.com/liutaocode/"
-                     f"talking-face-arxiv-daily/graphs/contributors\n"))
+            f.write((f"[contributors-url]: https://github.com/hhhh1138/"
+                     f"video-restoration-arxiv-daily/graphs/contributors\n"))
             f.write((f"[forks-shield]: https://img.shields.io/github/forks/liutaocode/"
                      f"talking-face-arxiv-daily.svg?style=for-the-badge\n"))
-            f.write((f"[forks-url]: https://github.com/liutaocode/"
-                     f"talking-face-arxiv-daily/network/members\n"))
+            f.write((f"[forks-url]: https://github.com/hhhh1138/"
+                     f"video-restoration-arxiv-daily/network/members\n"))
             f.write((f"[stars-shield]: https://img.shields.io/github/stars/liutaocode/"
                      f"talking-face-arxiv-daily.svg?style=for-the-badge\n"))
-            f.write((f"[stars-url]: https://github.com/liutaocode/"
-                     f"talking-face-arxiv-daily/stargazers\n"))
+            f.write((f"[stars-url]: https://github.com/hhhh1138/"
+                     f"video-restoration-arxiv-daily/stargazers\n"))
             f.write((f"[issues-shield]: https://img.shields.io/github/issues/liutaocode/"
                      f"talking-face-arxiv-daily.svg?style=for-the-badge\n"))
-            f.write((f"[issues-url]: https://github.com/liutaocode/"
-                     f"talking-face-arxiv-daily/issues\n\n"))
+            f.write((f"[issues-url]: https://github.com/hhhh1138/"
+                     f"video-restoration-arxiv-daily/issues\n\n"))
                 
     logging.info(f"{task} finished")        
 
